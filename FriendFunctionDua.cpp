bd7e25efe4bc539a077c71f55329ed243e68987d
RifalArdi25 <rifalardi25@gmail.com>
2025-05-05 19:41:13 +0700

    Melengkapi codingan

--- FriendFunctionDua.cpp
+++ FriendFunctionDua.cpp
@@ -2,4 +2,34 @@
 using namespace std;
 
 class pelajar;
-class manusia;
+class manusia
+{
+    public:
+    void showNilaiPelajar(pelajar &x);
+    void showNilaiPelajarDua(pelajar &x);
+};
+
+class pelajar{
+    private:
+    int nilai;
+
+    public:
+    pelajar() {nilai = 100; }
+    friend void manusia::showNilaiPelajar(pelajar &x);
+};
+
+void manusia::showNilaiPelajar(pelajar &x)
+{
+    cout << x.nilai;
+}
+void manusia::showNilaiPelajar(pelajar &y)
+{
+    cout << y.nilai;
+}
+
+int main (){
+    manusia budi;
+    pelajar pbudi;
+    budi.showNilaiPelajar(pbudi);
+    return 0;
+}
